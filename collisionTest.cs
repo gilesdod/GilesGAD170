5a52d57080403ba4e1f546f3d9d477958079cba1
Giles Dodsworth <giles.dodsworth@gmail.com>
2023-07-25 12:12:49 +1000

    GAD170 scripts from class

---
+++ collisionTest.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("OUCH");
+        if(collision.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Where are you going?");
+
+    }
+}
